a91d869e63ba2dcf27c67db8a639e2b82a700a2e
tatekitzhak <tatekosi@gmail.com>
2022-09-29 20:20:03 +0300

    Adde A new files project berry-react-mui-redux

---
+++ src/layout/MainLayout/index.js
@@ -0,0 +1,149 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+
+// material-ui
+import { styled, useTheme } from '@mui/material/styles';
+import { Paper, Typography, Container, AppBar, Box, CssBaseline, Toolbar, useMediaQuery } from '@mui/material';
+
+// project imports
+import Breadcrumbs from 'ui-component/extended/Breadcrumbs';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Customization from '../Customization';
+import navigation from 'menu-items';
+import { drawerWidth } from 'store/constant';
+import { SET_MENU } from 'store/actions';
+
+// assets
+import { IconChevronRight } from '@tabler/icons';
+
+function Footer(params) {
+    return (
+        /*  https://bit.cloud/eden/buy-a-goldfish/blocks/footer
+         <Layout>
+             <Footer logo="Goldfish inc.">
+                 <Footer.Column title="Features" items={["Hearing", "Vision"]} />
+                 <Footer.Column title="Resources" items={["Compare", "Blog"]} />
+                 <Footer.Column title="Social" items={["LinkedIn", "Instagram"]} />
+                 <Footer.Column title="Company" items={["About Us", "Careers"]} />
+             </Footer>
+         </Layout> */
+        <Paper sx={{ marginTop: 'calc(10%+60px)', bottom: 0 }} component="footer" square variant="outlined">
+            <Container maxWidth="lg">
+                <Box sx={{ flexGrow: 1, justifyContent: 'center', display: 'flex', my: 1 }}>
+                    <h1>Footer</h1>
+                </Box>
+                <Box sx={{ flexGrow: 1, justifyContent: 'center', display: 'flex', my: 2 }}>
+                    <Typography variant="caption" color="initial">
+                        Copyright ©2022. [] Limited
+                    </Typography>
+                </Box>
+                <Box sx={{ flexGrow: 1, justifyContent: 'center', display: 'flex', my: 1 }}>
+                    <h1>Footer</h1>
+                </Box>
+                <Box sx={{ flexGrow: 1, justifyContent: 'center', display: 'flex', my: 2 }}>
+                    <h1>Footer</h1>
+                </Box>
+            </Container>
+        </Paper>
+    );
+}
+
+// styles
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(({ theme, open }) => ({
+    ...theme.typography.mainContent,
+    ...(!open && {
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        }),
+        [theme.breakpoints.up('md')]: {
+            marginLeft: -(drawerWidth - 20),
+            width: `calc(100% - ${drawerWidth}px)`
+        },
+        [theme.breakpoints.down('md')]: {
+            marginLeft: '20px',
+            width: `calc(100% - ${drawerWidth}px)`,
+            padding: '16px'
+        },
+        [theme.breakpoints.down('sm')]: {
+            marginLeft: '10px',
+            width: `calc(100% - ${drawerWidth}px)`,
+            padding: '16px',
+            marginRight: '10px'
+        }
+    }),
+    ...(open && {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen
+        }),
+        marginLeft: 0,
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+        width: `calc(100% - ${drawerWidth}px)`,
+        [theme.breakpoints.down('md')]: {
+            marginLeft: '20px'
+        },
+        [theme.breakpoints.down('sm')]: {
+            marginLeft: '10px'
+        }
+    })
+}));
+
+// ==============================|| MAIN LAYOUT ||============================== //
+
+const MainLayout = () => {
+    const theme = useTheme();
+    const matchDownMd = useMediaQuery(theme.breakpoints.down('lg'));
+
+    // Handle left drawer
+    const leftDrawerOpened = useSelector((state) => state.customization.opened);
+    const dispatch = useDispatch();
+    const handleLeftDrawerToggle = () => {
+        dispatch({ type: SET_MENU, opened: !leftDrawerOpened });
+    };
+
+    useEffect(() => {
+        dispatch({ type: SET_MENU, opened: !matchDownMd });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [matchDownMd]);
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            {/* header */}
+            <AppBar
+                enableColorOnDark
+                position="fixed"
+                color="inherit"
+                elevation={0}
+                sx={{
+                    bgcolor: theme.palette.background.default,
+                    transition: leftDrawerOpened ? theme.transitions.create('width') : 'none'
+                }}
+            >
+                <Toolbar>
+                    <Header handleLeftDrawerToggle={handleLeftDrawerToggle} />
+                </Toolbar>
+            </AppBar>
+
+            {/* drawer */}
+            <Sidebar drawerOpen={leftDrawerOpened} drawerToggle={handleLeftDrawerToggle} />
+
+            {/* main content */}
+            <Main theme={theme} open={leftDrawerOpened}>
+                {/* breadcrumb */}
+                <Breadcrumbs separator={IconChevronRight} navigation={navigation} icon title rightAlign />
+                <Outlet />
+                <Footer />
+            </Main>
+            <Customization />
+        </Box>
+    );
+};
+
+export default MainLayout;
---
+++ src/store/constant.js
@@ -0,0 +1,5 @@
+import React from 'react';
+// theme constant
+export const gridSpacing = 3;
+export const drawerWidth = 260;
+export const appDrawerWidth = 320;
